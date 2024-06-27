b03c472d6f78829a2b3ba28a6e4a464e35f0d241
assistanceshiri <shendeanurag62@gmail.com>
2024-06-27 18:27:00 +0530

    Add files via upload
    Dictionary app using Html,Css ,Javascript

---
+++ Dictionary/app.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('form');
+const resultdiv = document.querySelector('.result');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault(); 
+    getWordinfo(form.elements[0].value);
+});
+
+const getWordinfo = async(word)=>{
+  try{
+    resultdiv.innerHTML ="Fetching data";
+    const response =await  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const data = await response.json();
+    let definitions = data[0].meanings[0].definitions[0];
+
+
+    resultdiv.innerHTML =`
+    <h2><strong>Word:</strong>${data[0].word}</h2>
+    <p class="speech" >${data[0].meanings[0].partOfSpeech}</p>
+    <p > <strong>Meaning:</strong>${definitions.definition === undefined ? "Not found":
+    definitions.definition }</p>
+     <p> <strong>Example:</strong>${definitions.example === undefined ? "NOt found" :
+    definitions.example}</p> 
+    <p><strong> Antonyms: <strong><P>
+     `;
+
+     if(definitions.antonyms.length === 0){
+        resultdiv.innerHTML+= `<span> Not Found </span>`;
+     }
+     else
+     {
+       for(let i=0; i<definitions.antonyms.length; i++){
+        resultdiv.innerHTML  += `<li>${definitions.antonyms[i]} </li>`
+       }
+     }
+     
+
+    resultdiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target =-blank">Read more</a></div>` 
+  }
+  catch (error){
+   resultdiv.innerHTML =`work are not found`;
+  }
+     
+}
